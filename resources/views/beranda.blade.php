1079315cbdfc3042fcfcddd7ff0649afd06caaad
galangrakasaputra <gagalangraka16@gmail.com>
2024-05-25 10:58:18 +0700

    push pertama

---
+++ resources/views/beranda.blade.php
@@ -0,0 +1,6 @@
+@extends('main')
+@section('content')
+    <div class="beranda" style="background-color: white; margin-top:12%; width:70%; margin-left:14%">
+        <img src="barang.jpg" alt="" style="">
+    </div>
+@endsection
---
+++ resources/views/support/navbar.blade.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Register</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
